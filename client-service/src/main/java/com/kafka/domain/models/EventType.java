31ed3c598778f8e6f7b21e3fa670f8cce9282b61
Reynaldo Cavero Rosales <rcaveror@emeal.nttdata.com>
2022-07-12 08:48:40 -0500

    proyecto04

--- client-service/src/main/java/com/kafka/domain/models/EventType.java
+++
@@ -1,5 +0,0 @@
-package com.kafka.domain.models;
-
-public enum EventType {
-  CREATED, UPDATED, DELETED
-}
--- notification-service/src/main/java/com/kafka/events/EventType.java
+++
@@ -1,5 +0,0 @@
-package com.kafka.events;
-
-public enum EventType {
-  CREATED, UPDATED, DELETED
-}
